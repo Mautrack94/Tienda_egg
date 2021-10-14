cbd18e3953b78d6e2adce32069683259cddeb68f
Mauro Lamia <mauro_lamia94@outlook.com>
2021-10-14 20:20:15 -0300

    Primer commit de la rama secundaria

---
+++ src/tienda/entidades/Distribuidor.java
@@ -0,0 +1,44 @@
+
+package tienda.entidades;
+
+
+public class Distribuidor {
+    private String nombre;
+    private String direccion;
+    private Integer id;
+
+    public Distribuidor() {
+    }
+
+    public Distribuidor(String nombre, String direccion, Integer id) {
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    
+    
+}
